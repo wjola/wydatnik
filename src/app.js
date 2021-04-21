ee80b11c452103a1f526805c59b8e9836577d928
Wjola <wioletta.czardybon@gmail.com>
2021-04-21 17:32:08 +0200

    Initial commit with configuration

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = <p>Koszty</p>;
+
+ReactDOM.render(app, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './public/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'public')
+    },
+    module: {
+        rules: [{
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  ['@babel/preset-env', { targets: "defaults" }]
+                ]
+              }
+            }
+        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+}
