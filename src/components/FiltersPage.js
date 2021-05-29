6e1beb7f35560c6a01ec11405c56d4a59120ebdb
Wjola <wioletta.czardybon@gmail.com>
2021-05-29 14:33:04 +0200

    Validate amount filter submission

--- src/components/FiltersPage.js
+++ src/components/FiltersPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { DateRangePicker } from 'react-dates';
 import moment from 'moment';
@@ -19,12 +19,49 @@ import {
 } from '../actions/filters';
 
 const FiltersPage = (props) => {
+    const [showAmountError, setShowAmountError] = useState(false);
     const [calendarFocus, setCalendarFocus] = useState(null);
+    const [amountFrom, setAmountFrom] = useState(props.filters.amountFrom);
+    const [amountTo, setAmountTo] = useState(props.filters.amountTo);
+    const history = useHistory();
+
+    const handleAmountFromChange = (amountFrom) => {
+        if (isAmountFormatValid(amountFrom)) {
+            setAmountFrom(amountFrom);
+        }
+    }
+
+    const handleAmountToChange = (amountTo) => {
+        if (isAmountFormatValid(amountTo)) {
+            setAmountTo(amountTo);
+        }
+    }
+
+    const handleFilterSubmit = () => {
+        if (amountFrom < amountTo || amountFrom === null || amountTo === null) {
+            !!amountFrom && props.setAmountFrom(amountFrom);
+            !!amountTo && props.setAmountTo(amountTo);
+            showAmountError && setShowAmountError(false);
+            history.push('/');
+        } else {
+            setShowAmountError(true);
+        }
+    }
+
+    const handleFilterCancel = () => {
+        history.push('/');
+    }
+
+    const isAmountFormatValid = (amount) => {
+        const regexp = /^\d+(\.)?\d?\d?$/;
+        
+        return amount.toString().match(regexp);
+    }
 
     return (
         <div>
             <Header />
-            <div className='filters__body'>
+            <div className='subpage__body'>
                 <h2 className='subpage__header'>Wybierz filtry</h2>
                 <fieldset className='filters__criterion'>
                     <legend className='paragraph--wide'>Podaj zakres kwot:</legend>
@@ -33,16 +70,16 @@ const FiltersPage = (props) => {
                         type='number'
                         name='amount'
                         id='amount-from'
-                        value={props.filters.amountFrom || ''}
-                        onChange={e => props.setAmountFrom(e.target.value)}
+                        value={amountFrom || ''}
+                        onChange={e => handleAmountFromChange(e.target.value)}
                     />
                     <input
                         className='input filters__input'
                         type='number'
                         name='amount'
                         id='amount-to'
-                        value={props.filters.amountTo || ''}
-                        onChange={e => props.setAmountTo(e.target.value)}
+                        value={amountTo || ''}
+                        onChange={e => handleAmountToChange(e.target.value)}
                     />
                 </fieldset>
                 <div className='filters__criterion'>
@@ -125,12 +162,18 @@ const FiltersPage = (props) => {
                     </fieldset>
                 </div>
                 <div className='filters__button-container'>
-                    <Link 
-                        to='/'
+                    <button 
+                        className='filter-button filter-button--light'
+                        onClick={handleFilterCancel}
+                    >
+                        Anuluj
+                    </button>
+                    <button 
                         className='filter-button filter-button--dark'
+                        onClick={handleFilterSubmit}
                     >
                         Zastosuj
-                    </Link>
+                    </button>
                 </div>
             </div>
             <Navigation />
