563975dfc6b613c1fbe7bddbc1ffec9dfefa6ad4
Wjola <wioletta.czardybon@gmail.com>
2021-08-23 20:54:53 +0200

    Make navlink to main page from header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import PigLogo from '../../images/piggy-bank-no-outline.svg';
 import useDeviceClass from '../utils/useDeviceClass';
 import DesktopNavigation from './DesktopNavigation';
@@ -8,10 +9,10 @@ const Header = () => {
     return (
         <div className='header-container'>
             <header className='header'>
-                <div className='header__logo'>
+                <NavLink to='/' className='header__logo'>
                     <img src={PigLogo} className='logo__pig logo__pig--small'/>
                     <h1 className='title'>Kosztopis</h1>
-                </div>
+                </NavLink>
                 {isDesktop && <DesktopNavigation />}
             </header>
         </div>       
